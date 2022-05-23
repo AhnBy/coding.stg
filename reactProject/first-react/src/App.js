3b2f832c3098867c3a32376d4a5e0e1e56d6d8a3
abc <qudcjf0701@naver.com>
2022-05-23 20:54:02 +0900

    lifecycle commit

--- reactProject/first-react/src/App.js
+++ reactProject/first-react/src/App.js
@@ -1,14 +1,16 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import './App.css';
 import Box from "./component/Box"
 function App() {
   let counter = 0;
   // useState(초기값) -> 반환값 = array [counter2 = 값, setCounter2 = 함수]
   const [counter2, setCounter2] = useState(0);
+  const [value, setValue] = useState(0);
   const increase =()=>{
     counter = counter + 1;
     // UI를 바로 바꾸는 것은 돈이 많이들기때문에 setCounter2(비동기작동)의 state값은 함수가 끝난 후 적용된다.
-    setCounter2(counter2 + 1);
+    //setCounter2(counter2 + 1);
+    setValue(value+2)
     console.log("counter는 ", counter, "counter2 state는 ", counter2);
   };
 
@@ -24,10 +26,22 @@ function App() {
 
   // 대부분의 것 = state
   // 잠깐 기억해야하는 값 = 변수
+  useEffect(()=>{
+    console.log("useEfferct1 Firee")
+  }, []);
+
+  useEffect(()=>{
+    console.log("useEfferct2 Firee", counter2, value)
+  }, [counter2]);
+
+  useEffect(()=>{
+    console.log("다른내용 하고싶어요", value)
+  }, [value]);
 
   return (
     //리액트에선 class 를 className 으로 사용한다.
   <div>
+    {console.log("render")}
     {/* component : 반복되는 Box를 만들어서 가져와서 쓴다. */}
     <Box name="리사" num={1}/>
     <Box name="제니" num={2}/>
