8976d878ffa94f7f3ed536efd91f5ecdf8417b8e
abc <qudcjf0701@naver.com>
2022-06-21 21:19:48 +0900

    hnm-project

---
+++ reactProject/hnm-react-router-practice/src/page/ProductAll.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react'
+import { Container, Row, Col, Alert } from 'react-bootstrap';
+import ProductCard from '../component/ProductCard';
+import { useSearchParams } from 'react-router-dom';
+
+const ProductAll = () => {
+    const [productList, setProductList] = useState([]);
+    const [query, setQuery] = useSearchParams();
+    let [error, setError] = useState("");
+    const getProducts = async () => {
+        try {
+        let searchQuery = query.get('q') || "";
+            let url = `http://localhost:5000/products?q=${searchQuery}`  //https://my-json-server.typicode.com/AhnBy/coding.stg
+            let response = await fetch(url);
+            let data = await response.json();
+            if (data.length < 1) {
+                if (searchQuery !== "") {
+                setError(`${searchQuery}와 일치하는 상품이 없습니다`);
+                } else {
+                throw new Error("결과가 없습니다");
+                }
+            }
+            setProductList(data);
+        } catch (err) {
+            setError(err.message);
+        }
+    }
+    useEffect(()=>{
+        getProducts()
+    },[query])
+  return (
+    <Container>
+        {error ? (
+            <Alert variant="danger" className="text-center">
+                {error} 
+            </Alert>
+        ) : (
+        <Row>
+            {productList.length > 0 && productList.map((item) => (
+                <Col md={3} sm={12} key={item.id}>
+                    <ProductCard item={item} />
+                </Col>
+            ))}
+        </Row>
+      )}
+    </Container>
+
+  )
+}
+
+export default ProductAll
---
+++ reactProject/hnm-react-router-practice/src/page/ProductDetail.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react'
+import { Container, Row, Col, Button, Dropdown, Alert } from 'react-bootstrap';
+import { useParams } from 'react-router-dom'
+
+const ProductDetail = () => {
+    let {id} = useParams();
+    const [product, setProduct] = useState(null);
+    const [loading, setLoading] = useState(false);
+    
+    const getProductDetail= async ()=>{
+        let url = `http://localhost:5000/products/${id}`; //https://my-json-server.typicode.com/AhnBy/coding.stg
+        let response = await fetch(url)
+        let data = await response.json();
+        console.log(data);
+        setLoading(false);
+        setProduct(data);
+    }
+    useEffect(()=>{
+        getProductDetail()
+    },[])
+    if(loading || product == null) return <h1>Loading</h1>
+  return (
+    <Container className='product-detail-card'>
+        <Row>
+            <Col className='product-img'>
+                <img src={product?.img} />
+            </Col>
+            <Col>
+                <div className='product-info'>{product?.title}</div>
+                <div className='product-info'>\ {product?.price}</div>
+                <div className='choice'>
+                    {product?.choice ? 'Conscius choice' : ''}
+                </div>
+                <Dropdown className='drop-down'>
+                    <Dropdown.Toggle variant='outline-dark' id='dropdown-basic'>사이즈 선택</Dropdown.Toggle>
+                    <Dropdown.Menu>{product?.size.length > 0 && product.size.map((item) => (
+                        <Dropdown.Item href='#/action-1'>{item}</Dropdown.Item>
+                    ))}</Dropdown.Menu>
+                </Dropdown>
+                <Button variant='dark' className='add-button'>
+                    추가
+                </Button>
+            </Col>
+        </Row>
+    </Container>
+  )
+}
+
+export default ProductDetail
