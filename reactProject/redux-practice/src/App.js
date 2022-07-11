d1b806c107624c19aa5d07f90d16052ee14c4a25
abc <qudcjf0701@naver.com>
2022-07-11 22:29:39 +0900

    REDUX COMMIT

---
+++ reactProject/redux-practice/src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import Box from './component/Box';
+
+function App() {
+  //const [count, setCount] = useState(0);
+  const count = useSelector(state=>state.count);
+  const dispatch = useDispatch();
+  let id = useSelector(state=>state.id)
+  let password = useSelector(state=>state.password)
+
+  const increase= ()=>{
+    dispatch({type:"INCREMENT", payload:{num:5}});
+    //setCount(count + 1);
+  }
+
+  const decrease= ()=>{
+    dispatch({type:"DECREMENT", payload:{num:5}});
+    //setCount(count + 1);
+  }
+
+  const login=()=>{
+    dispatch({type:"LOGIN", payload:{id:"noona", password: "123"}})
+  }
+
+  return (
+    <div>
+      <h1>{id},{password}</h1>
+      <h1>{count}</h1>
+      <button onClick={increase}>증가!</button>
+      <button onClick={login}>Login</button>
+      <button onClick={decrease}>감소!</button>
+      <Box/>
+    </div>
+  );
+}
+
+export default App;
